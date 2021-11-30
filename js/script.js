5865e8fbaff9bd81383fa3f503d32088cd32b46d
Luis Lechuga <luislechu55@gmail.com>
2021-11-29 20:37:08 -0600

    nuevo

--- js/script.js
+++ js/script.js
@@ -40,3 +40,67 @@ console.log(multiplicacion(5, 7));
 
 
 console.log("-----------------");
+
+var num = 0;
+
+for (let index = 0; index < 16; index++) {
+    num += 1;
+}
+
+console.log(num);
+
+function division() {
+
+    for (let index = 1; index < 17; index++) {
+       
+        //OPERADOR DE MODULO
+        if(index % 3 === 0 && index % 5 === 0) console.log('Fizz Buzz')
+        else if (index % 5 === 0) console.log('Buzz')
+        //else if (index % 15 === 0) {
+        else if (index % 3 === 0) console.log('fizz')
+        else console.log(index)
+
+    }
+}
+
+division()
+
+let resultado;
+
+for (let index = 1; index <= 16; index++) {
+    
+    if (index%3 == 0 && index%5 == 0) {
+        resultado = "FizzBuzz";
+    } else {
+        if (index%3 == 0) {
+            resultado = "Fizz";
+        }
+        else if (index%5 == 0) {
+            resultado = "Buzz";
+        } else {
+            resultado = index;
+        }
+    }
+    
+    document.write(index, " = ", resultado, "</br>");
+}
+
+function fizzFuzz() {
+    let listNumbers =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+
+    for(let i = 0; i < listNumbers.length; i++) {
+
+        let isFizz = listNumbers[i] % 3 === 0;
+        let isBuzz = listNumbers[i] % 5 === 0;
+
+        if(isFizz && isBuzz) {
+            console.log(listNumbers[i],'FizzBuzz');
+        } else if(isFizz) {
+            console.log(listNumbers[i],'Fizz');
+        } else if(isBuzz){
+            console.log(listNumbers[i],'Buzz');
+        }
+    }
+}
+fizzFuzz ();
