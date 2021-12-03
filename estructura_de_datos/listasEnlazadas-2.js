af1ea11fc6715c165bbc0a59343110c4df63967f
Luis Lechuga <luislechu55@gmail.com>
2021-12-02 19:30:00 -0600

    Lista

---
+++ estructura_de_datos/listasEnlazadas-2.js
@@ -0,0 +1,82 @@
+'use strict'
+
+class Node {
+    
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList{
+
+    constructor() {
+        this.head = null;
+    }
+
+    insertar(value) {
+        const node = new Node(value, this.head);
+        this.head = node;
+        console.log('INSERT ', this.head);
+    }
+
+    delete() {
+        if (this.head) {
+            if (this.head.next) {
+                const segundoNodo = this.head.next;
+                this.head = segundoNodo;
+            } else {
+                this.head = null;
+            }
+        }
+        console.log('DELETE ', this.head);
+    }
+
+    recorrido() {
+        let nodoActual = this.head;
+        while (nodoActual) {
+            console.log('RECORRIDO', nodoActual);
+            nodoActual = nodoActual.next;
+        }
+    }
+
+    busqueda(valor) {
+        /* 
+        ! simbolo de admiracion que cierra retorna el valor contrario
+        if > true o false
+        this.head tiene valores > true 
+        this.head no tiene valores > false 
+        !true == false
+        !false == true */
+
+        if (!this.head) {
+            return null
+        }
+
+        let nodoActual = this.head;
+
+        while (nodoActual) {
+            if (nodoActual.data === valor) {
+                return nodoActual.data;
+            }
+            nodoActual = nodoActual.next
+        }
+
+        return 'Valor no encontrado'
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.insertar('a');
+linkedList.insertar('b');
+linkedList.insertar('c');
+
+linkedList.recorrido();
+
+const valorEncontrado = linkedList.busqueda('c');
+console.log('valorEncontrado' , valorEncontrado);
+
+linkedList.delete();
+linkedList.delete();
+linkedList.delete();
